037ca530a878b0c1a985bdbc0f21cc3926751e0d
khoidt3 <khoidt3@gmail.com>
2019-04-28 15:00:37 +0700

    android update code fragment

---
+++ app/src/main/java/com/example/prm391x_tourguide_khoidtfx01411/CustomFragment.java
@@ -0,0 +1,18 @@
+package com.example.prm391x_tourguide_khoidtfx01411;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CustomFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.custom_fragment, container, false);
+        return view;
+    }
+}
