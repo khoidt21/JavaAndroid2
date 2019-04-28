5b814965f2cfc9fc75ee47f4c1139848da6301cc
khoidt3 <khoidt3@gmail.com>
2019-04-28 16:27:34 +0700

    update code

--- app/src/main/java/com/example/prm391x_tourguide_khoidtfx01411/ATMFragment.java
+++ app/src/main/java/com/example/prm391x_tourguide_khoidtfx01411/ATMFragment.java
@@ -1,16 +1,13 @@
 package com.example.prm391x_tourguide_khoidtfx01411;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-
+public class ATMFragment extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.atm_fragment);
     }
 }
--- app/src/main/java/com/example/prm391x_tourguide_khoidtfx01411/CustomFragment.java
+++ app/src/main/java/com/example/prm391x_tourguide_khoidtfx01411/CustomFragment.java
@@ -6,13 +6,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.content.Intent;
 
 public class CustomFragment extends Fragment {
+    ImageView imgHotel;
+    ImageView imgATM;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.custom_fragment, container, false);
+        imgHotel = view.findViewById(R.id.imgHotel);
+        imgHotel.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), HotelFragment.class);
+                startActivity(intent);
+            }
+        });
+        imgATM = view.findViewById(R.id.imgATM);
+        imgATM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ATMFragment.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
+
 }
