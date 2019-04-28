c3781c35e2d7ce044c72fd9cf410abfb0f5c7a5e
khoidt3 <khoidt3@gmail.com>
2019-04-28 23:21:50 +0700

    update code

--- app/src/main/java/com/example/prm391x_tourguide_khoidtfx01411/HotelFragment.java
+++ app/src/main/java/com/example/prm391x_tourguide_khoidtfx01411/HotelFragment.java
@@ -1,19 +1,27 @@
 package com.example.prm391x_tourguide_khoidtfx01411;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
-public class HotelFragment extends AppCompatActivity {
+public class HotelFragment extends Fragment {
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.hotel_fragment);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.hotel_fragment, container, false);
+
+        return view;
     }
 
 }
