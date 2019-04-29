90b0e8a4643f5951e9cc8507c3338b9981cf04a2
khoidt3 <khoidt3@gmail.com>
2019-04-29 18:04:24 +0700

    code update

--- app/src/main/java/com/example/prm391x_tourguide_khoidtfx01411/MainFragment.java
+++ app/src/main/java/com/example/prm391x_tourguide_khoidtfx01411/MainFragment.java
@@ -1,28 +1,20 @@
 package com.example.prm391x_tourguide_khoidtfx01411;
 
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
-public class CustomFragment extends Fragment {
+public class MainFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         super.onCreateView(inflater, container, savedInstanceState);
-        View view = inflater.inflate(R.layout.custom_fragment, container, false);
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
 
         return view;
     }
