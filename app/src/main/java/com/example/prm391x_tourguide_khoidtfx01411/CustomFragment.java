9fc7bbffa90349bce89cefa6d6f4e7a0af7fa2a3
khoidt3 <khoidt3@gmail.com>
2019-04-29 17:00:52 +0700

    update code

--- app/src/main/java/com/example/prm391x_tourguide_khoidtfx01411/CustomFragment.java
+++ app/src/main/java/com/example/prm391x_tourguide_khoidtfx01411/CustomFragment.java
@@ -1,7 +1,10 @@
 package com.example.prm391x_tourguide_khoidtfx01411;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -12,31 +15,17 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class CustomFragment extends FragmentActivity  {
+public class CustomFragment extends Fragment {
 
-    ImageView imgeHotel,imgeATM;
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.custom_fragment);
-        OnClickListener listener = new OnClickListener() {
-            public void onClick(View view) {
-                Fragment fragment = null;
-                if(view == findViewById(R.id.imgHotel)){
-                    fragment = new HotelFragment();
-                } else if(view == findViewById(R.id.imgATM)) {
-                    fragment = new ATMFragment ();
-                }
-                FragmentManager manager = getSupportFragmentManager();
-                FragmentTransaction transaction = manager.beginTransaction();
-                transaction.replace(R.id.customFragment, fragment);
-                transaction.commit();
-            }
-        };
-        imgeHotel = (ImageView) findViewById(R.id.imgHotel);
-        imgeHotel.setOnClickListener(listener);
-        imgeATM = (ImageView) findViewById(R.id.imgATM);
-        imgeHotel.setOnClickListener(listener);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.custom_fragment, container, false);
+
+        return view;
     }
 
 }
+
