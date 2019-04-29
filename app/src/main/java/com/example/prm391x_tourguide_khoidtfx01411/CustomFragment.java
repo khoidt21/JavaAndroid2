c2440a298378ad348f5d80ca324341a95250c14d
khoidt3 <khoidt3@gmail.com>
2019-04-29 14:44:54 +0700

    update code

--- app/src/main/java/com/example/prm391x_tourguide_khoidtfx01411/CustomFragment.java
+++ app/src/main/java/com/example/prm391x_tourguide_khoidtfx01411/CustomFragment.java
@@ -6,12 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.content.Intent;
 
 public class CustomFragment extends Fragment {
-    ImageView imgHotel;
-    ImageView imgATM;
 
     @Nullable
     @Override
@@ -19,23 +15,7 @@ public class CustomFragment extends Fragment {
 
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.custom_fragment, container, false);
-        imgHotel = view.findViewById(R.id.imgHotel);
-        imgHotel.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), HotelFragment.class);
-                startActivity(intent);
-            }
-        });
-        imgATM = view.findViewById(R.id.imgATM);
-        imgATM.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), ATMFragment.class);
-                startActivity(intent);
-            }
-        });
         return view;
     }
 
--- app/src/main/java/com/example/prm391x_tourguide_khoidtfx01411/MainActivity.java
+++ app/src/main/java/com/example/prm391x_tourguide_khoidtfx01411/MainActivity.java
@@ -3,13 +3,41 @@ package com.example.prm391x_tourguide_khoidtfx01411;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
-
+    ImageView imgHotel;
+    ImageView imgATM;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OnClickListener listener = new OnClickListener() {
+            public void onClick(View view) {
+                Fragment fragment = null;
+                if(view == findViewById(R.id.imgHotel)){
+                    fragment = new HotelFragment();
+                } else if(view == findViewById(R.id.imgATM)) {
+                    fragment = new ATMFragment();
+                }
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.customFragment, fragment);
+                transaction.commit();
+            }
+        };
+        imgHotel = (ImageView) findViewById(R.id.imgHotel);
+        imgHotel.setOnClickListener(listener);
+        imgATM  = (ImageView)findViewById(R.id.imgATM);
+        imgATM.setOnClickListener(listener);
     }
 }
